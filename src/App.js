d12370af897e0db042b3e4e4eba8cb1f0d26e05e
jheave <jimenezjheave123@gmail.com>
2022-09-05 16:28:38 +0800

    add did context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import {ChakraProvider} from "@chakra-ui/react";
+import React, {useState} from "react";
 import {Route, Routes} from "react-router-dom";
 import NavigationBar from "./components/NavBar";
+import {UserContext} from "./context/UserContext";
+import {DidContext} from "./context/DidContext";
 
 function App() {
+    const [user, setUser] = useState({
+        id: '',
+        username: '',
+        email: '',
+    });
+    const [did, setDid] = useState({did: ''});
     return (
-        <ChakraProvider>
-            <div>
-                <NavigationBar/>
-                <Routes>
-                    <Route path="/" element={<Dashboard/>}/>
-                    <Route path={"/sign-in"} element={<SignInForm/>}/>
-                    <Route path={"/request-certificate"} element={<Form/>}/>
-                    <Route path={"confirmation-code"} element={<OTP/>}/>
-                    <Route path={"/certificates"} element={<Certificate/>}/>
-                    <Route path={"/admin"} element={<Approver/>}/>
-                </Routes>
-            </div>
-        </ChakraProvider>
+        <UserContext.Provider value={{user, setUser}}>
+            <DidContext.Provider value={{did, setDid}}>
+                <div>
+                    <NavigationBar/>
+                    <Routes>
+                        <Route path="/" element={<Dashboard/>}/>
+                        <Route path={"/sign-in"} element={<SignInForm/>}/>
+                        <Route path={"/request-certificate"} element={<Form/>}/>
+                        <Route path={"confirmation-code"} element={<OTP/>}/>
+                        <Route path={"/certificates"} element={<Certificate/>}/>
+                        <Route path={"/admin"} element={<Approver/>}/>
+                    </Routes>
+                </div>
+            </DidContext.Provider>
+        </UserContext.Provider>
     );
 }
 
