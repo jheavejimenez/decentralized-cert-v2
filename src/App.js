dd5afb1e7a974921f1fadc185c1dc053524f8948
jheave <jimenezjheave123@gmail.com>
2022-09-05 16:20:33 +0800

    refractor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import {ChakraProvider} from "@chakra-ui/react";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-      <ChakraProvider>
-      </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <div>
+                <NavigationBar/>
+                <Routes>
+                    <Route path="/" element={<Dashboard/>}/>
+                    <Route path={"/sign-in"} element={<SignInForm/>}/>
+                    <Route path={"/request-certificate"} element={<Form/>}/>
+                    <Route path={"confirmation-code"} element={<OTP/>}/>
+                    <Route path={"/certificates"} element={<Certificate/>}/>
+                    <Route path={"/admin"} element={<Approver/>}/>
+                </Routes>
+            </div>
+        </ChakraProvider>
+    );
 }
 
 export default App;
